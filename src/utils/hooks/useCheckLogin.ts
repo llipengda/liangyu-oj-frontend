b018ad4cae9ff7b2ee0ab6cbcc4d5a602dd876e3
llipengda <lipengda2@outlook.com>
2023-11-28 02:05:21 +0800

    Fix conditional logic in useCheckLogin hook

--- src/utils/hooks/useCheckLogin.ts
+++ src/utils/hooks/useCheckLogin.ts
@@ -11,7 +11,7 @@ const useCheckLogin = () => {
   const dispatch = useAppDispatch()
 
   useEffect(() => {
-    if (needLogin && !isFirstRender) {
+    if (needLogin && isFirstRender) {
       dispatch(setOpenLogin(true))
       setIsFirstRender(false)
     }
