f4354a51e8b1ed64aeb0ea965caf209dc834a358
llipengda <lipengda2@outlook.com>
2023-11-28 00:44:48 +0800

    Add font-family and margin styles to App.css and update minHeight theme in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    minHeight: {
+      '1/4': '25vh',
+      '1/2': '50vh',
+      '3/4': '75vh'
+    },
     extend: {}
   },
   plugins: [],
